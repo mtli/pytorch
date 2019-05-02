979571528f96462b266268497aa2efa6b5f4cc9b
ilia-cher <iliacher@fb.com>
2019-05-01 22:29:35 -0700

    Move inter-op settings into ATen/Parallel
    Summary:
    Move inter-op settings into ATen/Parallel, abstract away explicit
    thread pool usage behind at::launch interface, python interface to
    setting number of threads
    
    Test Plan:
    BLAS=MKL USE_MKLDNN=1 USE_OPENCV=1 USE_FFMPEG=1 python setup.py develop --cmake
    
    ./build/bin/thread_init_test
    
    pytest -s -v test/test_torch.py::TestTorch
    
    gh-metadata: pytorch pytorch 20050 gh/ilia-cher/13/head

--- torch/csrc/jit/register_prim_ops.cpp
+++ torch/csrc/jit/register_prim_ops.cpp
@@ -18,6 +18,7 @@
 #include <torch/csrc/jit/script/logging.h>
 
 #include <ATen/ExpandUtils.h>
+#include <ATen/Parallel.h>
 #include <ATen/WrapDimUtils.h>
 #include <ATen/core/ivalue.h>
 #include <c10/core/thread_pool.h>
@@ -895,7 +896,7 @@ RegisterOperators reg(
 
              push(stack, forked_interprester.getFuture());
 
-             c10::global_work_queue().run(std::move(continuation));
+             at::launch(std::move(continuation));
              return 0;
            };
          }),
